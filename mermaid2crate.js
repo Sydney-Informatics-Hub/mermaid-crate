22690b8946ecf12b923af2e8b0ec712835c74764
Mike Lynch <m.lynch@sydney.edu.au>
2025-02-20 09:47:44 +1100

    Extremely simple version which builds a crate from a mermaid file which doesn't do anything that invokes DOMPurify

--- mermaid2crate.js
+++ mermaid2crate.js
@@ -7,6 +7,7 @@ import { hideBin } from "yargs/helpers";
 import mermaid from 'mermaid';
 import { ROCrate} from 'ro-crate';
 
+
 mermaid.initialize({
   startOnLoad: false,
 });
@@ -14,6 +15,7 @@ mermaid.initialize({
 
 async function mermaid2jsonld(mermaidGraph) {
 
+  // dig into mermaidAPI and look for a pre-sanitised version?
 
   const parser = (
     await mermaid.mermaidAPI.getDiagramFromText(mermaidGraph)
@@ -23,14 +25,30 @@ async function mermaid2jsonld(mermaidGraph) {
 
   const vertices = parser.getVertices();
 
-  for( const node of vertices ) {
-    crate.addEntity({
-      '@id': node.domId, 
-      '@type': node.type,
-      'name': node.id,
-      'description': node.label,
-    });
+  const nodes = {}
+
+  for( const [id, vertex] of parser.getVertices() ) {
+    nodes[id] = {
+      '@id': id, 
+      '@type': vertex.type,
+      'name': id,
+      'description': vertex.text,
+    };
   }
+  for( const edge of parser.getEdges() ) {
+    const source = edge.start;
+    const target = edge.end;
+    if( source in nodes ) {
+      nodes[source]["hasPart"] = { "@id": target }
+    } else {
+      throw(Error(`Edge has source id ${source} not found in vertex list`))
+    }
+  }
+
+  for( const nodeid in nodes ) {
+    crate.addEntity(nodes[nodeid]);
+  }
+
   return crate;
 }
 
@@ -39,8 +57,17 @@ async function main() {
     .usage("Usage: -i graph.mermaid").argv;
 
     const mermaidGraph = await promises.readFile(argv.i);
-    const crate = await mermaid2jsonld(mermaidGraph.toString());
-    //console.log(JSON.stringify(crate, null, 2));
+    try {
+      const crate = await mermaid2jsonld(mermaidGraph.toString());
+      console.log(JSON.stringify(crate, null, 2));
+    } catch(e) {
+      if( e.message.substr(0, 9) === "DOMPurify" ) {
+        console.error("mermaid on node can't handle lables because of DOMPurify");
+      } else {
+        console.error(`Couldn't parse mermaid file: ${e.message}`);
+      }
+    }
+
 }
 
 

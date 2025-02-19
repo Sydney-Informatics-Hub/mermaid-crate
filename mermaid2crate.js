a10f0bf0cadc04165a3e9c9a67ce17acebb325f5
Mike Lynch <m.lynch@sydney.edu.au>
2025-02-19 17:36:29 +1100

    Iterates over vertices to build crate entities

--- mermaid2crate.js
+++ mermaid2crate.js
@@ -3,52 +3,44 @@
 import { promises } from 'fs';
 import yargs from "yargs/yargs";
 import { hideBin } from "yargs/helpers";
+
 import mermaid from 'mermaid';
-
-// Define your Mermaid graph as a string
-const mermaidGraph = `
-graph TD;
-  A-->B-->C
-  A-->C
-`;
-
-async function l
+import { ROCrate} from 'ro-crate';
 
 mermaid.initialize({
   startOnLoad: false,
 });
 
-const parser = (
-  await mermaid.mermaidAPI.getDiagramFromText(mermaidGraph)
-).getParser().yy;
+
+async function mermaid2jsonld(mermaidGraph) {
 
 
-const vertices = parser.getVertices();
-const edges = parser.getEdges();
+  const parser = (
+    await mermaid.mermaidAPI.getDiagramFromText(mermaidGraph)
+  ).getParser().yy;
 
+  const crate = new ROCrate();
 
-console.log(vertices);
-console.log(edges);
+  const vertices = parser.getVertices();
+
+  for( const node of vertices ) {
+    crate.addEntity({
+      '@id': node.domId, 
+      '@type': node.type,
+      'name': node.id,
+      'description': node.label,
+    });
+  }
+  return crate;
+}
 
 async function main() {
   const argv = yargs(hideBin(process.argv))
-    .usage("Usage: -w WIDTH -o OUTPUT_PNG")
-    .default('w', 1200)
-    .default('o', 'poptimal.png').argv;
+    .usage("Usage: -i graph.mermaid").argv;
 
-  const svg = poptimal_svg(argv.w);
-  const opts = {
-      background: 'rgba(255, 255, 255, 1.0)',
-      fitTo: {
-          mode: 'width',
-          value: argv.w,
-      },
-    };
-    const resvg = new Resvg(svg, opts)
-  const pngData = resvg.render()
-    const pngBuffer = pngData.asPng()
-
-    await promises.writeFile(argv.o, pngBuffer);
+    const mermaidGraph = await promises.readFile(argv.i);
+    const crate = await mermaid2jsonld(mermaidGraph.toString());
+    //console.log(JSON.stringify(crate, null, 2));
 }
 
 
